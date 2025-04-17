54ca01b0edf12e7d026031bf89e8382c16ccf76a
Laiba Amjad <laiba.amjad9292@gmail.com>
2025-04-17 21:00:18 +0500

    add app colors and extension

---
+++ lib/utils/constants/app_colors.utils.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF143F40);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0x00000000);
+  static const white20 = Color(0x33FFFFFF);
+  static const primary30 = Color(0x4D143F40);
+  static const colorAAAAAA = Color(0x00AAAAAA);
+  static const color666666 = Color(0x00666666);
+  static const colorC0C0C0 = Color(0x00C0C0C0);
+  static const color888888 = Color(0x00888888);
+  static const color1C1B1B = Color(0x001C1B1B);
+  static const colorEE4D4D = Color(0x00EE4D4D);
+  static const color263238 = Color(0x00263238);
+  // static const color = Color(0x00);
+}
